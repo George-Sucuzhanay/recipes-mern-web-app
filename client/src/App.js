d144c32d92642939480308001eed10ba58ad02d6
George <georgesucuzhanay100@gmail.com>
2022-05-18 18:47:45 -0400

    installed tailwindcss and tested button components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='bg-black text-white text-3x1'>
+      Code Test
+      </div>
+      <div>
+          <Button variant="text" className="bg-sky-500">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+      </div>
     </div>
   );
 }
