9b277363f4b57b952d167cc46b4eb82541975e0d
George <georgesucuzhanay100@gmail.com>
2022-05-31 16:27:51 -0400

    changed scripts

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static('cilent/build'))
-}
 
 app.use(logger("dev"))
 
