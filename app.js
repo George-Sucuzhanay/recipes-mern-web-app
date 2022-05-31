0d96b02746d1369871505715b4833605cbaab760
George <georgesucuzhanay100@gmail.com>
2022-05-31 19:41:51 -0400

    behing commits

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const routes = require("./routes")
 const app = express()
 
 app.use(cors({origin: "https://6296a2cfcac1020009154ca8--adorable-axolotl-f72454.netlify.app/"}))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
