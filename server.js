e95ec26971fcf9265e7e1fd0ee8bcc2ca0b84480
George <georgesucuzhanay100@gmail.com>
2022-05-31 19:34:58 -0400

    cors erros for netlify

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express")
 const PORT = process.env.PORT || 3000
 
 
+
 db.on("error", console.error.bind(console, "MongoDB connection error"))
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
